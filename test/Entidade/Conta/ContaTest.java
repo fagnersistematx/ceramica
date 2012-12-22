6433bb1bc934240b3315b40960752d7890fe48b9
Fagner Silva <fagner.silva@dce.ufpb.br>
2012-12-22 12:27:56 -0300

    classe teste

---
+++ test/Entidade/Conta/ContaTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidade.Conta;
+
+import Controler.conta.contaReceitaController;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author fagner
+ */
+public class ContaTest {
+    
+    private Conta conta;
+    private contaReceitaController crc;
+    
+    public ContaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        conta = new Receita();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testConta() {
+        
+        
+        
+    }
+  
+}
