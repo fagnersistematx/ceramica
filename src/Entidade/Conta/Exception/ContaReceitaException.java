db24baba5548b21aecef00861d7450f0a9e8e50f
Fagner Silva <fagner.silva@dce.ufpb.br>
2012-12-23 14:07:44 -0300

    exceções de conta

---
+++ src/Entidade/Conta/Exception/ContaReceitaException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidade.Conta.Exception;
+
+/**
+ *
+ * @author fagner
+ */
+public class ContaReceitaException extends Exception {
+
+    /**
+     * Creates a new instance of
+     * <code>ContaReceitaException</code> without detail message.
+     */
+    public ContaReceitaException() {
+    }
+
+    /**
+     * Constructs an instance of
+     * <code>ContaReceitaException</code> with the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public ContaReceitaException(String msg) {
+        super(msg);
+    }
+}
