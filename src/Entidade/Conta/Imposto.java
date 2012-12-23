da9e13aad0d2e9214a8817cbd89eedf0c57e2648
Fagner Silva <fagner.silva@dce.ufpb.br>
2012-12-23 14:06:30 -0300

    classes conta imposto

---
+++ src/Entidade/Conta/Imposto.java
@@ -0,0 +1,77 @@
+package Entidade.Conta;
+
+
+
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Fagner Silva Martins, fagner.silva@dce.ufpb.br
+ * @version 1.0
+ */
+@Entity
+@Table(name="imposto")
+public class Imposto extends Conta implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private float valor;
+    private float aliquota;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public float getAliquota() {
+        return aliquota;
+    }
+
+    public void setAliquota(float aliquota) {
+        this.aliquota = aliquota;
+    }   
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Imposto)) {
+            return false;
+        }
+        Imposto other = (Imposto) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidade.Conta.Imposto[ id=" + id + " ]";
+    }
+
+}
